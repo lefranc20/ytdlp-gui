eeeaaf5121ee6c07ab4b4e09933bcc817872cd28
leFranc20 <84180970+lefranc20@users.noreply.github.com>
2023-05-26 19:12:51 -0400

    ADDS 26-05-2023
    Adds variantes + edições do .md

--- yt-dlp batch scripts/ytarc.bat
+++ yt-dlp batch scripts/ytarc.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set /p link="Digite um link para arquivar um canal do youtube:s "
+set /p link="Digite um link para arquivar um canal do youtube: "
 
 ::Arquivação da mais alta qualidade para mp4 de canais:
-yt-dlp -f bestvideo+bestaudio/bestvideo+bestaudio --merge-output-format mp4  --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json -o "%(uploader)s/%(title)s[%(upload_date)s][url_or_id_is - %(id)s].%(ext)s" %link%
+yt-dlp -f bestvideo+bestaudio/bestvideo+bestaudio --merge-output-format mp4  --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json -o "%%(uploader)s/%%(title)s[%%(upload_date)s][url_or_id_is - %%(id)s].%%(ext)s" %link%
---
+++ yt-dlp batch scripts/ytpls2.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo "Arquivacao da mais alta qualidade para mp4 de playlists"
+set /p link="Digite o link> "
+
+::Arquivação da mais alta qualidade para mp4 de playlists:
+yt-dlp -f bestvideo+bestaudio/bestvideo+bestaudio --merge-output-format mp4  --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json --no-abort-on-error -o "%%(playlist_title)s/%%(playlist_index)s-%%(title)s[%%(upload_date)s][url_or_id_is - %%(id)s].%%(ext)s" %link%
