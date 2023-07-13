822a5257067c98cfa51c8fc529fd5cac53d60ee5
bandolerofuzz <84180970+lefranc20@users.noreply.github.com>
2023-07-13 14:34:33 -0400

    Update ytpls2.bat

--- yt-dlp batch scripts/ytpls2.bat
+++ yt-dlp batch scripts/ytpls2.bat
@@ -3,5 +3,5 @@
 echo "Arquivacao da mais alta qualidade para mp4 de playlists"
 set /p link="Digite o link> "
 
-::Arquivação da mais alta qualidade para mp4 de playlists:
+::Arquivação em mp4 de playlists:
 yt-dlp -f mp4 --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json --no-abort-on-error -o "%%(playlist_title)s/%%(title)s[%%(upload_date)s][url_or_id_is - %%(id)s].%%(ext)s" %link%
