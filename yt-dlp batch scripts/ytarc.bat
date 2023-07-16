376517f50720b5587a08f89645e64c9f1149e49e
lefranc20 <84180970+lefranc20@users.noreply.github.com>
2023-07-16 12:54:43 -0400

    add

--- yt-dlp batch scripts/ytarc.bat
+++ yt-dlp batch scripts/ytarc.bat
@@ -3,4 +3,4 @@
 set /p link="Digite um link para arquivar um canal do youtube: "
 
 ::Arquivação da mais alta qualidade para mp4 de canais:
-yt-dlp -f bestvideo+bestaudio/bestvideo+bestaudio --merge-output-format mp4  --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json -o "%%(uploader)s/%%(title)s[%%(upload_date)s][%%(id)s].%%(ext)s" %link%
+yt-dlp -f bestvideo+bestaudio/bestvideo+bestaudio --merge-output-format mp4  --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json -o "%%(uploader)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s" %link%
