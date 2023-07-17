0ce10591452243f8fe32ae6530c115cea1f118d7
bandolerofuzz <84180970+lefranc20@users.noreply.github.com>
2023-07-17 15:12:35 -0400

    spacing alter

--- yt-dlp batch scripts/ytpls2.bat
+++ yt-dlp batch scripts/ytpls2.bat
@@ -4,4 +4,4 @@ echo "Arquivacao da mais alta qualidade para mp4 de playlists"
 set /p link="Digite o link> "
 
 ::Arquivação em mp4 de playlists:
-yt-dlp -f mp4 --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json --no-abort-on-error -o "%%(playlist_title)s/%%(title)s[%%(upload_date)s][url_or_id_is - %%(id)s].%%(ext)s" %link%
+yt-dlp -f mp4 --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json --no-abort-on-error -o "%%(playlist_title)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s" %link%
