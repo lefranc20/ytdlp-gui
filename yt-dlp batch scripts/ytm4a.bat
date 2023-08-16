8fa5586ac263480c953de08ac7da1c8f31c71946
bandolerofuzz <84180970+lefranc20@users.noreply.github.com>
2023-08-16 16:14:56 -0400

    Create ytm4a.bat

---
+++ yt-dlp batch scripts/ytm4a.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo "Musica no formato m4a em 128k"
+set /p link="Digite o link para baixar um audio no formato m4a em 128k: "
+
+::Música no formato m4a em 128k:
+yt-dlp -f bestaudio --extract-audio --audio-format m4a --audio-quality 128k --embed-metadata --embed-thumbnail -o "%%(title)s [%%(id)s].%%(ext)s" %link%
