4f9784f280c0c62a68819ceff4187a4dc2f26e40
bandolerofuzz <84180970+lefranc20@users.noreply.github.com>
2023-10-20 13:00:02 -0400

    Comentários e echo
    Comentários e echo alterados nos ytpls.

--- yt-dlp batch scripts/ytplsb2.bat
+++ yt-dlp batch scripts/ytplsb2.bat
@@ -1,6 +1,8 @@
 @echo off
 
+echo "Download de playlists na melhor qualidade possivel em um mp4"
+echo "Formato: %%(playlist_title)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s"
 set /p link="Digite o link> "
 
-::Arquivação da mais alta qualidade para mp4 de playlists:
+::Download de playlists da mais alta qualidade em um mp4:
 yt-dlp -f bestvideo+bestaudio/bestvideo+bestaudio --merge-output-format mp4  --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json --no-abort-on-error -o "%%(playlist_title)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s" %link%
--- yt-dlp batch scripts/ytsclSINGLE.bat
+++ yt-dlp batch scripts/ytsclSINGLE.bat
@@ -1,6 +1,7 @@
 @echo off
 
 echo "Musica no music.youtube ou Soundcloud (O LIMITE E POR VOLTA DE 192k (ACHO) ou as vezes 160k)"
+echo "Formato: %%(uploader)s - %%(title)s.%%(ext)s"
 set /p link="Digite o link para baixar um album no music.youtube/artista no soundcloud: "
 
 ::Musicas unicas no music.youtube ou Soundcloud:
