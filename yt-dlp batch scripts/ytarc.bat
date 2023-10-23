f7303590ebcb96de255bbca66bed9ca345442790
lefranc <leonardofranciscocustodio@gmail.com>
2023-10-23 13:15:58 -0400

    Teste

--- yt-dlp batch scripts/ytarc.bat
+++ yt-dlp batch scripts/ytarc.bat
@@ -5,3 +5,11 @@ set /p link="Digite um link para arquivar um canal do youtube: "
 
 ::Arquivação de uma qualidade qualquer em mp4 de um canal completo:
 yt-dlp -f mp4 --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json -o "%%(uploader)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s" %link%
+
+
+
+
+
+
+
+TESTE
