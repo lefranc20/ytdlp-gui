8a9019cb9c64c1ef535dd23062f3597b505c300b
lefranc <leonardofranciscocustodio@gmail.com>
2023-10-27 13:46:42 -0400

    Alterações na estrutura Adição de uma pasta main.

--- yt-dlp batch scripts/ytarc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo "Arquivação numa qualidade pré-definida"
-
-echo Formato: %%(uploader)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s
-set /p link="Digite um link para arquivar um canal do youtube: "
-
-::Arquivação de uma qualidade qualquer em mp4 de um canal completo:
-yt-dlp -f mp4 --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json -o "%%(uploader)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s" %link%
--- yt-dlp batch scripts/ytpls2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo "Download de playlists na qualidade pré-definida"
-echo "Formato: %%(playlist_title)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s"
-set /p link="Digite o link> "
-
-::Download de playlists no formato mp4:
-yt-dlp -f mp4 --all-subs --embed-subs --write-annotations --write-comments --embed-thumbnail --write-thumbnail --embed-metadata --embed-chapters --write-description --write-info-json --no-abort-on-error -o "%%(playlist_title)s/%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s" %link%
