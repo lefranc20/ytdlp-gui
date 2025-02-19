d596cca3a355114863a428b792a3e6ba34d400b9
leonardo <leonardofranciscocustodio@gmail.com>
2025-02-19 14:13:25 -0400

    Remoção de arquivos não utilizados

--- scripts/templates-windows/ytmp4.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-::set /p link="> "
-
-echo "Baixar um video de uma qualidade pré-definida em mp4"
-set /p link="Digite o link> "
-
-
-yt-dlp -f mp4 --embed-metadata --embed-chapters --all-subs --embed-subs --embed-thumbnail -o "%%(title)s [%%(upload_date)s] [%%(id)s].%%(ext)s" %link%
